1e19a2970d3d8f77e6736b1ba6fa2d36c5b54d5b
krystn <92931538+krystineee@users.noreply.github.com>
2026-07-15 23:18:24 +0800

    Remove unnecessary package declaration

--- Tasks/Task1.java
+++ Tasks/Task1.java
@@ -1,10 +1,8 @@
-package Tasks;
-
-public class Task1 {
 /** 
  * The Task1.java implements an application that
  * simply prints "Kamusta Mundo!" to standard output.
  */
+public class Task1 {
     public static void main(String[] args) {
         System.out.println("Kamusta Mundo!");  // Display the string.
     }
