48ca481ef8f69ae6ee1f4af5ffa0de1bc67ee07d
shibaa <92931538+krystineee@users.noreply.github.com>
2026-07-15 13:47:14 +0800

    Task1.java
    Implement an application that prints "Kamusta Mundo!"

---
+++ Tasks/Task1.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+public class Task1 {
+/** 
+ * The Task1.java implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+    public static void main(String[] args) {
+        System.out.println("Kamusta Mundo!");  // Display the string.
+    }
+}
