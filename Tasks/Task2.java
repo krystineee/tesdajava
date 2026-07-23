98cb203f2f1d6f3a8b73a5f189f06c3ba4347db0
krystn <92931538+krystineee@users.noreply.github.com>
2026-07-23 04:19:10 +0000

    Task2: Primitive data types concatenation exercise

---
+++ Tasks/Task2.java
@@ -0,0 +1,16 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        byte three = 3;
+        short one = 1;
+        int zero = 0;
+        float fTwo = 2.0f;
+        boolean bol = true;
+        char cH = 'H', cW = 'w', cR = 'r', cD='d';
+
+       String output = "" + cH + three + one + one + zero + " "
+            + cW + zero + cR + one + cD + " "
+            + fTwo + " " + bol;
+        System.out.println(output);
+    }
+}
